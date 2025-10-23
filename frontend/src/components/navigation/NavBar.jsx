8ea63da010add1c8167987c48560ea9d21f011d3
Malar5717 <malarmariam613@gmail..com>
2025-10-23 22:23:03 +0530

    navbar logic written

--- frontend/src/components/navigation/NavBar.jsx
+++ frontend/src/components/navigation/NavBar.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './NavBar.css'
 
 function NavBar({ onAddClick, isCreateOpen }) {
+
+  const [ isVerified, setIsVerified ] = useState(false)
+
+  useEffect(() => {
+    const isAuth = () => {
+      axios.get("http://localhost:3000/user/protected", {withCredentials: true})
+        .then(res => setIsVerified(res.data.isVerified))
+        .catch(err => console.log(err))
+    }
+    isAuth()
+  }, [])
+
   return (
     <div className='navbar_main'>
       <div className='logo'>
         <p>LOGO</p>
       </div>
       <div className='account'>
-        <p onClick={() => onAddClick(!isCreateOpen)}>add</p>
-        <p>acc</p>
+        {isVerified && 
+          <>
+            <p onClick={() => onAddClick(!isCreateOpen)}>create</p>
+            <p>my memories</p>
+          </>
+        }
+    
+        {!isVerified &&
+          <>
+            <p>signup</p>
+            <p>login</p>
+          </>
+        }
       </div>
     </div>
   )
