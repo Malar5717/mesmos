023fb0e7d31a6b26ea716887e92f6c95ee3e1c09
Malar5717 <malarmariam613@gmail..com>
2026-02-06 04:19:33 +0530

    catch-all middlewares added

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,46 @@
-// entry point of backend 
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const user_router = require('./routes/UserRoutes');
 const pola_router = require('./routes/PolaRoutes');
-const cookieparser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 
-// express is the web framework
 const app = express();
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
-app.use(express.json());
-app.use(cookieparser());
+app.use(helmet());
+app.use(express.json({ limit: '50kb' }));
+app.use(cookieParser());
 
-// connect is a Promise, connect then listen - "connected?"
-const mongoDB = process.env.MONGODB_URL; 
-mongoose.connect(mongoDB).then(()=> {
-    app.listen(3000,()=>{
-        console.log("connected to DB");
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server running on port 3000");
+            console.log("Connected to MongoDB");
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); 
     });
+
+app.use('/user', user_router);
+app.use('/pola', pola_router);
+
+app.get('/', (req, res) => {
+    res.json({ msg: "running" });
 });
 
-// parent route for all 
-app.use('/user',user_router);
-app.use('/pola',pola_router);
-app.get('/',(req, res)=>{
-    res.json({msg:"hello"})
-})
+app.use((req, res) => {
+    res.status(404).json({ msg: "route not found" });
+});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ msg: "Server error. Please try again." });
+});
 
